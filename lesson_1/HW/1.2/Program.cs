25e5db7849f29ac68990ab4b9b61e0f1b1be5384
Aleksey <lexa111_88@mail.ru>
2023-03-09 20:59:29 +0300

    HW1_2

---
+++ lesson_1/HW/1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+
+Console.Write("Write a number1 n1 = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write a number2 n2 = ");
+num2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write a number1 n3 = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+if (num1 < num2)
+{
+    num1 = num2;
+}
+if (num1 < num3)
+{
+    num1 = num3;
+}
+
+Console.Write($"Номер {num1} наибольшее");
