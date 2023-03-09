372c06297be8a007884ac8ec9407017378332291
Aleksey <lexa111_88@mail.ru>
2023-03-09 20:09:54 +0300

    HW1_1

---
+++ lesson_1/HW/1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿int num1 = 0;
+int num2 = 0;
+
+Console.Write("Write a number1 n1 = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write a number2 n2 = ");
+num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.Write(num1);
+    Console.Write(" -> max");
+    
+}
+else if (num1 < num2)
+{
+     Console.Write(num2);
+     Console.Write(" -> max");
+   
+}
+else 
+{
+    Console.Write(num1);
+    Console.Write(" -> равно");
+}
