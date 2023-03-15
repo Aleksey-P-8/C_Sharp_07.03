6e3f858eb4ebc2e739622931a960f6b7cbbc8d60
Aleksey <lexa111_88@mail.ru>
2023-03-15 21:14:36 +0300

    HW 2_3

--- lesson_2/HW/1.1/Program.cs
+++ lesson_2/HW/1.1/Program.cs
@@ -1,4 +1,4 @@
-﻿string NumSec(int num)
+﻿ NumSec(int num)
 {
     if ((-1000 < num && num < -100) || (num < 1000 && num > 100))
         return $"{num / 10 % 10}";
---
+++ lesson_2/HW/2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100)
+{
+Console.WriteLine("there is no third digit");
+return;
+}
+
+while (num > 999) num /= 10;
+Console.WriteLine(num % 10);
+}
+
+int val = int.Parse(Console.ReadLine()!);
+ThirdNum(val);
