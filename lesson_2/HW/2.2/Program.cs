e6e13c46ee638ba4b984561089f69b45bfb9f242
Aleksey <lexa111_88@mail.ru>
2023-03-15 20:51:31 +0300

    HW 2_2

---
+++ lesson_2/HW/2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string SecondNum(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+    return $"{num} -> {num / 100 * 10 + num % 10}";
+    return "The number is not three-digut";
+}
+
+int val = int.Parse(Console.ReadLine()!);
+string result = SecondNum(val);
+Console.WriteLine(result);
