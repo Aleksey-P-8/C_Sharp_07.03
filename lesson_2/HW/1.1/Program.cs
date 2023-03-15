917965fbe96c487e65beba04bee09e772a6204bb
Aleksey <lexa111_88@mail.ru>
2023-03-15 21:25:14 +0300

    HW 2_4

--- lesson_2/HW/1.1/Program.cs
+++ lesson_2/HW/1.1/Program.cs
@@ -1,8 +1,8 @@
-﻿ NumSec(int num)
+﻿string NumSec(int num)
 {
-    if ((-1000 < num && num < -100) || (num < 1000 && num > 100))
-        return $"{num / 10 % 10}";
-    return "The number is not three-digut";
+    if ((-1000 < num && num < -100) || (num < 1000 && num >100))
+    return $"{num / 10 % 10}";
+return "The number is not three-digut";
 }
 
 Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
---
+++ lesson_2/HW/2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void Weekend(int num)
+{
+    string text = "not quite a week";
+
+    if (num == 7 || num == 6) text = "weekend";
+    else if (num < 6 && num > 0) text = "workday";
+
+    Console.WriteLine($"{num} -> {text}");
+}
+
+int val = int.Parse(Console.ReadLine()!);
+Weekend(val);
